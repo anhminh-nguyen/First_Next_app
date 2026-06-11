4c0c7f27710df20b050ab451a5298642d5b14117
ketza-am <ketzamonicabes@gmail.com>
2026-06-11 16:25:11 -0400

    Second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const page = () => {
   return (
-    <div>page</div>
+    <div >
+      <h1 className="h1-bold text-center text-violet-600 " >Welcome back</h1>
+      <h1 className="h1-bold text-center text-violet-600 font-space-grotesk " >Welcome back</h1>
+    </div>
   )
 }
 export default page
+
